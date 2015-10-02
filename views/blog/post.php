9199532aa932a7e04660f3dbef56505058c6aa54
Anne <anne@yootheme.com>
2015-10-02 17:48:39 +0200

    updated post, posts and comments view

--- views/blog/post.php
+++ views/blog/post.php
@@ -1,19 +1,23 @@
 <?php $view->script('post', 'blog:app/bundle/post.js', 'vue') ?>
 
-<article class="uk-article">
+<div class="tm-container-small">
 
-    <?php if ($image = $post->get('image.src')): ?>
-    <div class="tm-article-teaser uk-display-block"><img src="<?= $image ?>" alt="<?= $post->get('image.alt') ?>"></div>
-    <?php endif ?>
+    <article class="uk-article">
 
-    <h1 class="uk-article-title"><?= $post->title ?></h1>
+        <?php if ($image = $post->get('image.src')): ?>
+        <div class="tm-article-teaser uk-display-block"><img src="<?= $image ?>" alt="<?= $post->get('image.alt') ?>"></div>
+        <?php endif ?>
 
-    <p class="uk-article-meta">
-        <?= __('Written by %name% on %date%', ['%name%' => $post->user->name, '%date%' => '<time datetime="'.$post->date->format(\DateTime::ISO8601).'" v-cloak>{{ "'.$post->date->format(\DateTime::ISO8601).'" | date "longDate" }}</time>' ]) ?>
-    </p>
+        <h1 class="uk-article-title"><?= $post->title ?></h1>
 
-    <div class="uk-margin"><?= $post->content ?></div>
+        <p class="uk-article-meta">
+            <?= __('Written by %name% on %date%', ['%name%' => $post->user->name, '%date%' => '<time datetime="'.$post->date->format(\DateTime::ISO8601).'" v-cloak>{{ "'.$post->date->format(\DateTime::ISO8601).'" | date "longDate" }}</time>' ]) ?>
+        </p>
 
-    <?= $view->render('blog/comments.php') ?>
+        <div class="uk-margin"><?= $post->content ?></div>
 
-</article>
+        <?= $view->render('blog/comments.php') ?>
+
+    </article>
+
+</div>
