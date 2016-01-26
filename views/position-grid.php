971b35e9756380c152436726a7856e092bd528fe
Anne <anne@yootheme.com>
2016-01-26 17:00:32 +0100

    fixed widget classes

--- views/position-grid.php
+++ views/position-grid.php
@@ -11,7 +11,7 @@
 
     ?>
 
-    <div class="uk-panel <?= $widget->theme['panel'] ?><?= $widget->theme['alignment'] ? ' uk-text-center' : '' ?><?= $contrast ? ' uk-contrast' : '' ?>">
+    <div class="uk-panel <?= $widget->theme['panel'] ?> <?= $widget->theme['alignment'] ? 'uk-text-center' : '' ?><?= $contrast ? ' uk-contrast' : '' ?> <?= $widget->theme['html_class'] ?>">
 
         <?php if (!$widget->theme['title_hide']) : ?>
         <h3 class="<?= $widget->theme['title_size'] ?>"><?= $widget->title ?></h3>
